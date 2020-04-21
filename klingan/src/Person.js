2f25a29016f0734eb3cfc031624463983f260316
Vargen18 <45463154+vargen18@users.noreply.github.com>
2020-04-21 11:05:36 +0200

    Renamed hello-world (project folder) to klingan

---
+++ klingan/src/Person.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Person(WrappedComponent) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        age: 0
+      this.incrementAge = this.incrementAge.bind(this)
+      }
+
+      incrementAge(){
+        this.setState({
+          age: this.state.age + 1;
+        });
+      }
+
+      render(){
+        return (
+          <div>
+            <label>My age is: {this.state.age}</label>
+            <button onClick={this.incrementAge}>Grow me older !!<button>
+        </div>
+      );
+    }
+  }
+  }
+
+  export default Person;
