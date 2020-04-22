8425883c4e2c7974c571926713f87508225b341a
Anna Nilsson <nilsanna@net.chalmers.se>
2020-04-22 09:59:54 +0200

    Added a product element
    Static placeholder

---
+++ klingan/src/Components/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Product(props) {
+  return (
+    <div>
+      <span>You thought it was juice, but it was I DIO</span>
+    </div>
+  );
+}
+
+export default Product;
