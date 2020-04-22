d2300ca4f8181e0798056c514221ea9739c04b84
Anna Nilsson <nilsanna@net.chalmers.se>
2020-04-22 10:00:45 +0200

    Added List element to display products
    Dynamic display, but static as the product elements need to be hardcoded into the list.

---
+++ klingan/src/Components/ProductList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./Product";
+
+function ProductList(props) {
+  return (
+    <div>
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+export default ProductList;
