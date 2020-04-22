bea87ecf37d38ccdc08a93243863f3bcad58ef13
Anna Nilsson <nilsanna@net.chalmers.se>
2020-04-22 10:49:05 +0200

    Removed customer information

--- klingan/src/Components/ProductList.js
+++ klingan/src/Components/ProductList.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Product from "./Product";
-import CustomerInfromation from "./CustomerInformation";
 
 function ProductList(props) {
   return (
     <div>
-      <CustomerInfromation name="Börge" />
       <Product name="Potatis" />
       <Product name="Lösgodis" />
       <Product name="Frysta Grönsaker" />
