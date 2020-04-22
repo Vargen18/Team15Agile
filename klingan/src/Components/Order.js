70d82e03807c1664f348097867e11c23dd52fcca
Anna Nilsson <nilsanna@net.chalmers.se>
2020-04-22 10:48:46 +0200

    Added order that handles product list and Customer information

---
+++ klingan/src/Components/Order.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CustomerInfromation from "./CustomerInformation";
+import ProductList from "./ProductList";
+
+function Order(props) {
+  return (
+    <div>
+      <CustomerInfromation />
+      <ProductList />
+    </div>
+  );
+}
+
+export default Order;
+
+//produ
