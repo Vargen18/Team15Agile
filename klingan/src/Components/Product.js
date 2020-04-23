0fb1a8dd3e9750056b87da502a03afc50f605a64
Johannes Kvernes <johannes.kvernes@gmail.com>
2020-04-23 14:35:01 +0200

    Converted Product into class

--- klingan/src/Components/Product.js
+++ klingan/src/Components/Product.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Product(props) {
-  return (
-    <div>
-      <span>You thought it was {props.name}, but it was I DIO</span>
-    </div>
-  );
+class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { product: props.name };
+  }
+
+  render() {
+    return (
+      <div>
+        <span>You thought it was {this.state.product}, but it was I DIO</span>
+      </div>
+    );
+  }
 }
 
 export default Product;
