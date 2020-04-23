8c64bc7bd76770c4222a91902893b7be44d69b66
Johannes Kvernes <johannes.kvernes@gmail.com>
2020-04-23 11:35:50 +0200

    Slight formatting

--- klingan/src/Components/CustomerInformation.js
+++ klingan/src/Components/CustomerInformation.js
@@ -4,8 +4,11 @@ function CustomerInfromation(props) {
   return (
     <div>
       <span>Kundens namn: {props.name}</span>
+      <br />
       <span>Mailadress: {props.mail}</span>
+      <br />
       <span>Telefonnummer: {props.telefon}</span>
+      <br />
     </div>
   );
 }
