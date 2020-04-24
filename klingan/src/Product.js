29e6fc5f569992ee4d2bebd5a5736c2e98cb7f1c
Vargen18 <45463154+vargen18@users.noreply.github.com>
2020-04-24 13:44:17 +0200

    css fix
    No longer applies the Product style to span

--- klingan/src/Product.js
+++ klingan/src/Product.js
@@ -8,16 +8,14 @@ import "./Product.css";
 class Product extends React.Component {
     render() {
         return (
-            <div>
-                <span>
-                    <img
-                        src={this.props.url}
-                        width="200"
-                        height="200"
-                        alt="new"
-                    />
+            <div className="Product">
+                <img
+                    src={this.props.url}
+                    width="200"
+                    height="200"
+                    alt="new"
+                />
                     <h3>{this.props.name}</h3>
-                </span>
             </div>
         );
     }
