7ecaffa3a05642c99add621694d0fe7c8b346211
Oliver Karmetun <oliver.k@live.se>
2020-05-05 10:02:41 +0200

    Add: Implement OrderProductListIcon

--- klingan/src/Components/OrderProductList.js
+++ klingan/src/Components/OrderProductList.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import ProductListIcon from "./ProductListIcon";
+import OrderProductListIcon from "./OrderProductListIcon";
 
 // A class that holds a list of products, to be used in the Order component.
 // The products are displayed after one another with breaks in between.
 // Param: list of products.
-class ORderProductList extends Component {
+class OrderProductList extends Component {
   constructor(props) {
     super(props);
     this.state = { products: props.products };
