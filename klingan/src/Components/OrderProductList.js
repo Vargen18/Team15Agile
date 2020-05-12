312fe9a947f4afeb34faa1d3f7a7f46a5a845bfa
Johannes Kvernes <johannes.kvernes@gmail.com>
2020-05-12 14:40:32 +0200

    Updated documentation

--- klingan/src/Components/OrderProductList.js
+++ klingan/src/Components/OrderProductList.js
@@ -6,7 +6,7 @@ import ListGroup from "react-bootstrap/ListGroup";
 import * as db from "../database/Database";
 
 // A class that holds a list of products, to be used in the Order component.
-// The products are displayed after one another with breaks in between.
+// The products are sorted and displayed per section.
 // Param: list of products.
 class OrderProductList extends Component {
   constructor(props) {
