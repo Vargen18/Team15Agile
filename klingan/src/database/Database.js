5981077d762c77183ccc62bcf4e628617e2c97a8
Anna Nilsson <nilsanna@net.chalmers.se>
2020-05-18 15:51:19 +0200

    Fixed order sorting by product group instead of sections

--- klingan/src/database/Database.js
+++ klingan/src/database/Database.js
@@ -4,52 +4,45 @@
 
 export function getProducts() {
   return [
-    // { id: 0, section: "", name: "Ris", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Brun_ris.jpg/1280px-Brun_ris.jpg" },
-    // { id: 1, section: "", name: "Yoghurt", url: "https://webcomicms.net/sites/default/files/clipart/143564/yoghurt-pictures-143564-9861608.jpg" },
-    // { id: 2, section: "", name: "Nocco", url: "https://www.tingstad.com/fixed/images/Main/1570702879/21366260.png" },
-    // { id: 3, section: "", name: "Bärs", url: "http://1.bp.blogspot.com/-3xy02lE8HnM/UY0z7NvGpVI/AAAAAAAAGqw/mTmitY-MF2o/s1600/bild-2.JPG" },
-    // { id: 4, section: "", name: "Kaviar", url: "https://webcomicms.net/sites/default/files/clipart/143564/yoghurt-pictures-143564-9861608.jpg" },
-    // { id: 5, section: "", name: "Vodka", url: "https://upload.wikimedia.org/wikipedia/commons/6/65/Absolut_vodka.jpg" },
-    // { id: 6, section: "", name: "Mjöl", url: "https://www.bohuslaningen.se/image/policy:1.6591695:1529070430/Aklh7j2p4Dpk1OWXu8tvW-h2ZWg.jpg?f=Regular&w=960&$p$f$w=7e0687f" },
     {
       id: 7,
       section: "Bröd",
-      name: "Surdegsbröd",
+      name: "Bröd hårt",
       url:
         "https://bakeassetsprod.blob.core.windows.net/editorial/2016/01/Knadfritt-surdegsbrod.jpg",
     },
     {
       id: 8,
       section: "Bröd",
-      name: "Knäckebröd",
+      name: "Bröd mjukt",
       url:
         "https://upload.wikimedia.org/wikipedia/commons/5/5a/Knaeckebroed.jpg",
     },
     {
       id: 9,
       section: "Mejeri",
-      name: "Västerbottenost",
+      name: "Påläggsost",
       url:
         "https://cdn.natmat.se/content/shopimages/large/54256-200063000000001735.jpg",
     },
     {
       id: 10,
       section: "Frukt och grönt",
-      name: "Spenat",
+      name: "Apelsiner",
       url:
         "https://upload.wikimedia.org/wikipedia/commons/3/36/Spinach_produce-1.jpg",
     },
     {
       id: 11,
       section: "Kött",
-      name: "Bacon",
+      name: "Kött fryst",
       url:
         "https://vignette.wikia.nocookie.net/bacon/images/3/33/Buy_pork_streaky_bacon_from_online_butcher.jpg/revision/latest/scale-to-width-down/340?cb=20110507120600",
     },
     {
       id: 12,
       section: "Fryst",
-      name: "Ärtor",
+      name: "Potatis fryst",
       url:
         "https://upload.wikimedia.org/wikipedia/commons/1/11/Peas_in_pods_-_Studio.jpg",
     },
@@ -63,21 +56,21 @@ export function getProducts() {
     {
       id: 14,
       section: "Mejeri",
-      name: "Laktosfri mjölk",
+      name: "Ägg",
       url:
         "https://www.valio.se/sites/default/files/styles/list_small/public/field/image/product/mjolk_latt_eko_1.png?itok=JMxNE1NA",
     },
     {
       id: 15,
       section: "Fryst",
-      name: "Fryst kyckling",
+      name: "Bär fryst",
       url:
         "https://assets.icanet.se/t_product_large_v1,f_auto/7318690118053.jpg",
     },
     {
       id: 16,
       section: "Kött",
-      name: "Skinka",
+      name: "Gris",
       url:
         "https://melins.se/wp-content/uploads/2018/01/skinka-skivad-20181102-097A4515.jpg",
     },
@@ -104,3 +97,19 @@ export function getCustomer2() {
 export function getSections() {
   return ["Mejeri", "Frukt och grönt", "Fryst", "Bröd", "Kött"];
 }
+
+export function getProductGroups() {
+  return [
+    "Gris",
+    "Potatis fryst",
+    "Apelsiner",
+    "Äpple",
+    "Bröd hårt",
+    "Bär fryst",
+    "Ägg",
+    "Kött fryst",
+    "Påläggsost",
+    "Bröd mjukt",
+  ];
+}
+//Innan du frågar, ja de finns två "Äpple"
