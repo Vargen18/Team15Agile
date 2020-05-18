d830aad8cca4077e4bba49c5e8c11c4e2535aeac
Oliver Karmetun <oliver.k@live.se>
2020-05-18 10:13:30 +0200

    Add: "Sort" field to ProductListIcon

--- klingan/src/Components/ProductListIcon.js
+++ klingan/src/Components/ProductListIcon.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 // Class that provides a card for a product containing the picture and name.
 // Also buttons for increasing/decresing the number of products in the shopping cart.
@@ -18,7 +22,8 @@ class ProductListIcon extends Component {
       kund: props.kund,
       section: props.section,
       addProd: props.addProd,
-      removeProd: props.removeProd
+      removeProd: props.removeProd,
+      sortRef: React.createRef(),
     };
 
     this.increaseClick = this.increaseClick.bind(this);
@@ -30,7 +35,7 @@ class ProductListIcon extends Component {
       name: this.state.product.name,
       url: this.state.product.url,
       units: 1,
-      section: this.state.section
+      section: this.state.section,
     });
   }
 
@@ -41,11 +46,39 @@ class ProductListIcon extends Component {
   render() {
     return (
       <div class="card" style={{ width: "13rem" }}>
-        <img class="card-img-top" src={this.state.picture} alt="Card image cap" height="200px" />
+        <img
+          class="card-img-top"
+          src={this.state.picture}
+          alt="Card image cap"
+          height="200px"
+        />
+        <Form>
+          <Form.Group as={Row} controlId="formHorizontalNamn">
+            <Col sm={10}>
+              <Form.Control
+                type="sort"
+                placeholder="Specifik sort"
+                ref={this.state.sortRef}
+              />
+            </Col>
+          </Form.Group>
+        </Form>
         <div class="card-body">
           <h5 class="card-title">{this.state.productName}</h5>
-          <button type="button" class="btn btn-success btn-lg" onClick={this.increaseClick}>+</button>
-          <button type="button" class="btn btn-danger btn-lg" onClick={this.decreaseClick}>-</button>
+          <button
+            type="button"
+            class="btn btn-success btn-lg"
+            onClick={this.increaseClick}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger btn-lg"
+            onClick={this.decreaseClick}
+          >
+            -
+          </button>
         </div>
       </div>
     );
